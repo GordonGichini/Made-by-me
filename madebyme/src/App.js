c05700aa22aeb745d596a7d26ad84506a9b4defc
GordonGichini <gordongichini1@gmail.com>
2023-01-27 12:43:18 +0300

    components added

---
+++ madebyme/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { BrowseRouter } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return ( 
+      <BrowseRouter>
+      <div className="App">
+        <h1>Made by me</h1>
+      </div>
+      </BrowseRouter>
+    );
+  }
+}
+
+export default App;
