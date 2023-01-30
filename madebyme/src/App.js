70acc995d4e919f816a86353e6e31cc3c4fd1f7a
GordonGichini <gordongichini1@gmail.com>
2023-01-30 22:01:48 +0300

    create project form added

--- madebyme/src/App.js
+++ madebyme/src/App.js
@@ -4,7 +4,8 @@ import Navbar from "./components/layout/Navbar";
 import Dashboard from './components/Dashboard/Dashboard' 
 import ProjectDetails from "./components/projects/ProjectDetails"; 
 import SignIn from "./components/Auth/SignIn"; 
-import SignUp from './components/Auth/SignUp';
+import SignUp from './components/Auth/SignUp'; 
+import CreateProject from "./components/projects/CreateProject";
 
 
 class App extends Component {
@@ -17,7 +18,8 @@ class App extends Component {
           <Route path='/' element={<Dashboard />} /> 
           <Route path='/project/:id' element={<ProjectDetails />} /> 
           <Route path='/signin' element={<SignIn />} /> 
-          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signup' element={<SignUp />} /> 
+          <Route path='/create' element={<CreateProject />} />
         </Routes>
       </div>
     </BrowserRouter>
